aadedecbaefe4b6b04dacca6e144c1551b4a3cc6
Kingsley Solomon <kinxly@gmail.com>
2023-09-11 13:40:53 +0100

    Initial commit

---
+++ src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.scss';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="moviebox" />
+    </div>
+  );
+}
+
+export default App;
